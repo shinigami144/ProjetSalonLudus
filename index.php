d6ac5a98f89205eb8a46059038cc759fd12492c2
Shini <d.abraham@ludus-academie.com>
2020-11-10 17:45:24 +0100

    Ajout de index.php different
    Different pour les teste de git

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File</title>
+</head>
+<body>
+    <p>
+        vous êtes sur le git test si vous avez déjà vu master alors tous fonctionne
+    
+    
+    </p>
+</body>
+</html>';
+
+?>
