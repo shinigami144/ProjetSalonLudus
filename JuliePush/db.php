ceec3354ae502c82b7520b1052c74c46225ec942
Shini <d.abraham@ludus-academie.com>
2020-11-30 12:23:20 +0100

    Mise enplace

--- JuliePush/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "salonvirtuel";
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
